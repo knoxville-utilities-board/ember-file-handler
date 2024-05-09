05bab6bd36fab2bb6ae817da2432b68ee26a11ae
Tyler Senter <tyler.senter.427@gmail.com>
2024-05-09 09:27:05 -0400

    fix: Transform TypeScript during build (#11)
    * fix: Transform TypeScript during build
    
    * chore: Bump package version
    
    * chore: Add exports to index
    
    * chore: Update package version

---
+++ addon/index.ts
@@ -0,0 +1,12 @@
+export type { FileTransferOptions } from './types';
+
+export {
+  default as FileTransfer,
+  GET,
+  POST,
+  TYPE_TEXT,
+  TYPE_ARRAY_BUFFER,
+  TYPE_BLOB,
+  TYPE_DOCUMENT,
+  TYPE_JSON,
+} from './file-transfer';
--- addon/types.d.ts
+++ addon/types.d.ts
@@ -10,5 +10,26 @@ export type RequestData =
   | (() => RequestData | Promise<RequestData>);
 export type TransformedRequestData = XMLHttpRequestBodyInit | null | undefined;
 
+export type FileTransfer = {
+  isUpload: boolean;
+  isProcessing: boolean;
+  isSuccessful: boolean;
+  progress: number;
+  loaded: number;
+  total: number;
+  response: ResponseData | null | undefined;
+  withHeader(key: string, value: string): FileTransfer;
+  withQueryParams(
+    params: URLSearchParams | Record<string, string> | null
+  ): FileTransfer;
+  withData(data: RequestData): FileTransfer;
+  withResponseType(type: ResponseType): FileTransfer;
+  abort(): void;
+  preview: string | URL | RequestData | null | undefined;
+  begin(): Promise<ResponseData | null | void>;
+};
+export type FileTransferOptions = {
+  method?: HttpMethod;
+};
 export type ResponseData = ArrayBuffer | Blob | Document | object | string;
 export type ResponseType = XMLHttpRequestResponseType;
